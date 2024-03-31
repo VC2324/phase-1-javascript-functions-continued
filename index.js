2ad3fc7f2cd37dc7bdcfa922343fc99cef2ca638
Victor <victorac2324@gmail.com>
2024-03-30 20:40:32 -0400

    used advanced functions to access inner function values

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+function saturdayFun(study = "roller-skate"){
+    return `This Saturday, I want to ${study}!`
+}
+saturdayFun()
+
+const mondayWork = function(rest="go to the office"){
+    return `This Monday, I will ${rest}.`
+}
+
+function wrapAdjective(string = "*"){
+    return function inner (spec = "special"){
+        return `You are ${string}${spec}${string}!`
+
+
+
+    }
+}
+wrapAdjective("%")("a dedicated programmer")
